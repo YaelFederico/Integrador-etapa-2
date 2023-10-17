e3fb8999249290219d9a0922e7c070d9841bdf25
YaelFederico <Federicogonzalo2000@hotmail.com>
2023-10-16 23:13:06 -0300

    agrego detalles a la carpeta pages

--- src/pages/Nosotros.jsx
+++ src/pages/Nosotros.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import "./Nosotros.scss";
 
 const Nosotros = () => {
   return (
-    <div>Nosotros</div>
+    <>
+
+
+      <div className="section-nosotros">
+        
+        <h2 className="section-nosotros__title">Sobre BOXing Store Argentina</h2>
+
+        <p className="section-nosotros__p"> Somos deportes de lucha, somos calidad y protección para el artista
+        marcial, el boxeador profesional, amateur y el aficionado que esta
+        comenzando en el camino del deporte de contacto.</p>
+
+        <p className="section-nosotros__p">Desde la primera venta hasta la ultima buscamos la calidad y el confort que no podes encontrar en otra tienda de argentina, tenemos lo que buscas. SOMOS BOXing Store ARGENTINA.</p>
+        
+      </div>
+      </>
   )
 }
 
